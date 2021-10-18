913c18ea44fef29690fa1a553b079d561fd998f8
ballsona <gongsona@gmail.com>
2021-10-19 02:24:57 +0900

    날씨앱완료/투두앱시작

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { View,Text,StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+import { theme } from './color';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const work = () => setWorking(true);
+  const travel = () => setWorking(false);
   return (
+    //Header
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+            <Text style={{...styles.btnText,color: working? "white":theme.grey}}>Work</Text>
+        </TouchableOpacity >
+         <TouchableOpacity onPress={travel}>
+          <Text style={{ ...styles.btnText, color: working ? theme.grey : "white" }}>Travel</Text>
+        </TouchableOpacity>
+      </View>
+      {/* <StatusBar style="white" /> */}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    paddingHorizontal:30
   },
-});
+  header: {
+    flexDirection: "row",
+    marginTop: 60,
+    justifyContent:"space-between"
+  },
+  btnText: {
+    fontSize: 30,
+    fontWeight: "700",
+    color: "white"
+  }
+})
